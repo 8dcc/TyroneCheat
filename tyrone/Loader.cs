aae81bbb9349e26839cfd1b9e86e78cd4d250dec
8dcc <8dcc.git@gmail.com>
2023-04-18 21:31:48 +0200

    Initial commit

---
+++ tyrone/Loader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace tyrone
+{
+    public class Loader
+    {
+        public static void Init()
+        {
+            Loader.Load = new GameObject();
+
+            Loader.Load.AddComponent<Main>();
+            Loader.Load.AddComponent<Renderer>();
+
+            UnityEngine.Object.DontDestroyOnLoad(Loader.Load);
+        }
+
+        public static void Unload()
+        {
+            _Unload();   
+        }
+
+        private static void _Unload()
+        {
+            GameObject.Destroy(Loader.Load);
+        }
+
+        private static GameObject Load;
+    }
+}
---
+++ tyrone/Main.cs
@@ -0,0 +1,41 @@
+﻿using Steamworks;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace tyrone 
+{
+    internal class Main : MonoBehaviour
+    {
+        vFPSController fpscontroller = FindObjectOfType<vFPSController>();
+        vp_FPWeapon fpweapon = FindObjectOfType<vp_FPWeapon>();
+
+        public void Start()
+        {
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.End))
+            {
+                Loader.Unload();
+            }
+
+            fpscontroller.gravity = 0.5f;
+
+            fpweapon.BobAmplitude = Vector3.zero;
+            fpweapon.BobRate = Vector3.zero;
+            fpweapon.ShakeAmplitude = Vector3.zero;
+            fpweapon.ShakeSpeed = 0.0f;
+        }
+
+        public void OnGUI()
+        {
+            /* Watermark settings */
+            Vector2 pos = new Vector2(10f, 10f);
+            string str = "TyroneHakZ420 (8dcc)";
+
+            Renderer.DrawString(pos, str, new Color(0.9f, 0.9f, 0.9f), false);
+        }
+    }
+}
